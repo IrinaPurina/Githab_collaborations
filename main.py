c5dd5e7db1b27ce77ac572bc27b9f87bcc6e664a
IrinaPurina <llolla94@gmail.com>
2017-05-23 02:32:37 +0500

    add graphviz

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 from github import Github
 
-users = ["kostaNew", "koshinus", "arkaev", "Genraim", "CLearERR", "gogoleff", "denchistyakov", "Zhigalov", "trixartem", "mokhov"]
+# users = ["kostaNew", "koshinus", "arkaev", "Genraim", "CLearERR", "gogoleff", "denchistyakov", "Zhigalov", "trixartem", "mokhov"]
+users = ["kostaNew", "koshinus"]
 
-gitHub = Github()
+def get_collaborations(users):
+    gitHub = Github()
 
-projects = []
+    projects = []
+    for user in users:
+        repos = gitHub.get_user(user).get_repos()
+        for repo in repos:
+            if repo.fork:
+                projects.append(repo.source)
+            else:
+                projects.append(repo)
 
-for user in users:
-    repos = gitHub.get_user(user).get_repos()
-    for repo in repos:
-        if repo.fork:
-            projects.append(repo.source)
-        else:
-            projects.append(repo)
+    collaborations = {}
+    for project in projects:
+        contributors = project.get_contributors()
+        interesting_contributors = [contributor for contributor in contributors if users.count(contributor.login) > 0]
+        if len(interesting_contributors) > 1:
+            collaborations[project.full_name] = interesting_contributors
 
-collaborations = {}
+    return collaborations
 
-for project in projects:
-    contributors = project.get_contributors()
-    interestingContributors = [contributor for contributor in contributors if users.count(contributor.login) > 0]
-    if len(interestingContributors) > 1:
-        collaborations[project.full_name] = interestingContributors;
-        print(project.full_name)
-        print("----------------------")
-        for contributor in contributors:
-            print(contributor.login)
+collaborations = get_collaborations(users)
+
+for key in collaborations:
+    contributors = collaborations[key]
+    print(key)
+    for contributor in contributors:
+        print(contributor.login)
+
