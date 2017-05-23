43613553fb4e6056950e3099e861545f1b02f827
IrinaPurina <llolla94@gmail.com>
2017-05-23 22:47:38 +0500

    remove duplicate projects

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
 from github import Github
 from graphviz import Graph
 
-users = ["kostaNew", "koshinus", "Genraim", "CLearERR", "gogoleff", "denchistyakov", "Zhigalov", "mokhov"]
+users = ["kostaNew", "koshinus", "gogoleff", "denchistyakov", "Zhigalov", "mokhov"]
 
 def get_collaborations(users):
     github = Github()
 
-    projects = []
+    projects = {} 
     for user in users:
-        repos = github.get_user(user).get_repos()
+        repos = github.get_user(user).get_repos() 
         for repo in repos:
             if repo.fork:
-                projects.append(repo.source)
+                projects[repo.source.full_name] = repo.source
             else:
-                projects.append(repo)
+                projects[repo.full_name] = repo
 
     collaborations = {}
-    for project in projects:
-        contributors = project.get_contributors()
-        interesting_contributors = [contributor for contributor in contributors if users.count(contributor.login) > 0]
-        if len(interesting_contributors) > 1:
-            collaborations[project.full_name] = interesting_contributors
+    for key in projects:
+        project = projects[key]
+        contributors = project.get_contributors() 
+        interesting_contributors = [contributor for contributor in contributors if users.count(contributor.login) > 0] 
+        if len(interesting_contributors) > 1: 
+            collaborations[project.full_name] = interesting_contributors 
 
-    return collaborations
+    return collaborations 
 
 
 def render_collaborations(collaborations):
-    dot = Graph(comment="Collaborations")
+    dot = Graph() 
 
     for user in users:
-        dot.node(user, user)
+        dot.node(user, user) 
 
-    dot.attr("node", style="filled", color="lightgrey")
+    dot.attr("node", style="filled", color="lightgrey") 
+    
     for key in collaborations:
         contributors = collaborations[key]
-        dot.attr("node", group=key)
+        
         dot.node(key, key)
         for contributor in contributors:
             dot.edge(contributor.login, key)
